898c8a3f9ef7bf7d001a033ce270ed42a83cd284
Lucia Cossaro <luciacossaro@Lucias-MBP.lan>
2023-10-04 21:41:20 +0200

    update notebooks

--- cours_python_upec/cours_python_upec/PyBooks/PyParis_XII_M2/cours/hello_world.py
+++
@@ -1,4 +0,0 @@
-nom = input("Entrez votre nom : ")
-print()
-print("Hello world !")
-print(f"Salut {nom}, bienvenue dans ce cours de Python !")
--- cours_python_upec/cours_python_upec/PyBooks/PyParis_XII_M2/cours/slides/B.1.2_ComposantsBase.run.bat
+++
@@ -1,2 +0,0 @@
-%homedrive%%homepath%\Anaconda\python jupyter-nbconvert-img.py ..\B.1.2_ComposantsBase.ipynb --to slides --post serve --SlidesExporter.reveal_scroll=True
-pause
